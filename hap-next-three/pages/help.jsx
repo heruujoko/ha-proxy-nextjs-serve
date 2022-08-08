b5979dfdf6e7c15fc237d00bf8e71b50760d4be8
Heru Joko P. Utomo <heru.utomo@grabtaxi.com>
2022-08-08 16:13:26 +0800

    init example ha-proxy docker-compose

---
+++ hap-next-three/pages/help.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelpPage = () => {
+    return (
+        <>
+            <h1>Help page from instance ONE</h1>
+        </>
+    )
+}
+
+export default HelpPage;
